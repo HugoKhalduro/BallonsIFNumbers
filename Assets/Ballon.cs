f367fcce5f3d28310cae72f5080c43667ffc17a9
HugoKhalduro <hugo.lucario@hotmail.com>
2020-02-22 15:24:07 -0300

    Update Ballon.cs
    Correção no Balão, agora ele explode e troca a animação na mesma posição.

--- Assets/Ballon.cs
+++ Assets/Ballon.cs
@@ -3,37 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ballon : MonoBehaviour
-{   
-    public TextMesh Pontuacao;
-    private int pontos;
+{
+
     public GameObject BlowingBalloon;
     public float force = 10f;
     Rigidbody2D rig;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        pontos = 0;
-        Pontuacao.text = "Pontos: " + pontos;
         rig = GetComponent<Rigidbody2D>();
         rig.AddForce(transform.up * force, ForceMode2D.Impulse);
     }
 
-    void OnTriggerEnter2D(Collider2D coll){
-        if(coll.tag == "Finger"){
-
-            Vector2 direction = (coll.transform.position - transform.position).normalized;
-            Quaternion rotation = Quaternion.LookRotation(direction);
-
-            GameObject blowingBalloon = Instantiate(BlowingBalloon, transform.position, rotation);
-            Destroy(blowingBalloon, 2f);
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Finger")){
+            GameObject blowingBallon = Instantiate(BlowingBalloon, coll.gameObject.transform.position, Quaternion.identity);
+            Destroy(blowingBallon, 0.5f);
             Destroy(gameObject);
+        }
+        
 
-        }
-        if(tag == "Finger"){
-            pontos = pontos + 1;
-            Pontuacao.text = "Pontos: " + pontos;
-        }
     }
 }
